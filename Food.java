710f029c635368f445dc1254a709e26d47cc7f84
firomsa <firomsassf@gmail.com>
2023-11-05 00:24:36 +0300

    second

--- Food.java
+++ Food.java
@@ -3,7 +3,7 @@ import java.awt.Color;
 import javax.swing.JLabel;
 
 public class Food extends JLabel {
-    public static int numberOfFood = 320;
+    public static int numberOfFood = 318;
     Food(int x,int y){
         setBounds(x+5,y+5,10,10);
         setBackground(Color.ORANGE);
--- Pacman.java
+++ Pacman.java
@@ -1,2 +1,12 @@
-public class Pacman{
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public class Pacman extends JLabel{
+    public int[] startPos = {14,24};
+    Pacman(){
+        Icon imgIcon = new ImageIcon(this.getClass().getResource("Pacman.gif"));
+        setIcon(imgIcon);
+        setBounds(startPos[1]*20, startPos[0]*20+60, 40, 40);
+    }
 }
